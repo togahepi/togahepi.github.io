8e85e3f07754da099e20c1e2248b5fce96c95df8
togahepi <44219605+togahepi@users.noreply.github.com>
2018-11-27 22:03:23 +0700

    if

---
+++ Day_28/baiTapDieuKien.js
@@ -0,0 +1,11 @@
+//Bai 1
+//Test case
+//
+
+function bai1() {
+	var a = prompt("Nhap vao tham so thu nhat");
+	var b = prompt("Nhap vao tham so thu hai");
+	if (a == NaN) {
+		return document.write("So thu nhat la so!");
+	}
+}
---
+++ Day_28/condition.js
@@ -0,0 +1,20 @@
+var i = "Anh yêu em";
+
+if (typeof i == "number") {
+	document.write("Thank you for watching");
+}
+
+function soSanhChuoi() {
+	"a" > "b";
+	return soSanhChuoi();
+}
+
+function yeuHayKhongYeu() {
+	var x = "Em có yêu anh đâu?";
+	if (x = true) {
+		document.write("Sao em lại phũ thế?");
+	}
+}
+
+
+
